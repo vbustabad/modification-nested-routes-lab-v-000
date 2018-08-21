dc5d87bf771510adc3c5c8aa4b824abf9f292c9a
Veronica Bustabad <vbustabad@gmail.com>
2018-08-21 03:38:22 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,7 +28,7 @@ class SongsController < ApplicationController
     if params[:artist_id] && !Artist.exists?(params[:artist_id])
         redirect_to artists_path, alert: "Artist not found."
     else
-      @song = Song.new(author_id: params[:author_id])
+      @song = Song.new(artist_id: params[:artist_id])
     end
   end
 
